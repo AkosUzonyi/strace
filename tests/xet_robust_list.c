6345e6b921826ac36597b171cd98b23ec1f85347
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-06-24 14:39:40 +0200

    xset_robust_list

--- tests/xet_robust_list.c
+++ tests/xet_robust_list.c
@@ -8,6 +8,7 @@
 
 #include "tests.h"
 #include "scno.h"
+#include "pidns.h"
 
 #if defined __NR_get_robust_list && defined __NR_set_robust_list
 
@@ -30,28 +31,32 @@ sprintaddr(void *addr)
 int
 main(void)
 {
-	const pid_t pid = getpid();
+	pidns_test_init();
+
+	const pid_t pid = pidns_ids[PT_TGID];
 	const long long_pid = (unsigned long) (0xdeadbeef00000000LL | pid);
 	TAIL_ALLOC_OBJECT_CONST_PTR(void *, p_head);
 	TAIL_ALLOC_OBJECT_CONST_PTR(size_t, p_len);
 
 	if (syscall(__NR_get_robust_list, long_pid, p_head, p_len))
 		perror_msg_and_skip("get_robust_list");
-	printf("get_robust_list(%d, [%s], [%lu]) = 0\n",
-	       (int) pid, sprintaddr(*p_head), (unsigned long) *p_len);
+	pidns_printf("get_robust_list(%s, [%s], [%lu]) = 0\n",
+	       pidns_pid2str(PT_TGID), sprintaddr(*p_head),
+	       (unsigned long) *p_len);
 
 	void *head = tail_alloc(*p_len);
 	if (syscall(__NR_set_robust_list, head, *p_len))
 		perror_msg_and_skip("set_robust_list");
-	printf("set_robust_list(%p, %lu) = 0\n",
+	pidns_printf("set_robust_list(%p, %lu) = 0\n",
 	       head, (unsigned long) *p_len);
 
 	if (syscall(__NR_get_robust_list, long_pid, p_head, p_len))
 		perror_msg_and_skip("get_robust_list");
-	printf("get_robust_list(%d, [%s], [%lu]) = 0\n",
-	       (int) pid, sprintaddr(*p_head), (unsigned long) *p_len);
+	pidns_printf("get_robust_list(%s, [%s], [%lu]) = 0\n",
+	       pidns_pid2str(PT_TGID), sprintaddr(*p_head),
+	       (unsigned long) *p_len);
 
-	puts("+++ exited with 0 +++");
+	pidns_printf("+++ exited with 0 +++\n");
 	return 0;
 }
 
