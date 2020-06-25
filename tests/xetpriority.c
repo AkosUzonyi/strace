3a091db652c71fe4ee073df7cc111d91470fbde7
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-06-25 16:21:07 +0200

    xetpriority

--- tests/xetpriority.c
+++ tests/xetpriority.c
@@ -7,6 +7,7 @@
 
 #include "tests.h"
 #include "scno.h"
+#include "pidns.h"
 
 #if defined __NR_getpriority && defined __NR_setpriority
 
@@ -17,16 +18,27 @@
 int
 main(void)
 {
-	const int pid = getpid();
+	pidns_test_init();
+
+	const int pid = pidns_ids[PT_TGID];
+	const int pgid = pidns_ids[PT_PGID];
+
 	long rc = syscall(__NR_getpriority, PRIO_PROCESS,
 			  F8ILL_KULONG_MASK | pid);
-	printf("getpriority(PRIO_PROCESS, %d) = %ld\n", pid, rc);
+	pidns_printf("getpriority(PRIO_PROCESS, %s) = %ld\n",
+		pidns_pid2str(PT_TGID), rc);
 
 	rc = syscall(__NR_setpriority, PRIO_PROCESS,
 		     F8ILL_KULONG_MASK | pid, F8ILL_KULONG_MASK);
-	printf("setpriority(PRIO_PROCESS, %d, 0) = %s\n", pid, sprintrc(rc));
+	pidns_printf("setpriority(PRIO_PROCESS, %s, 0) = %s\n",
+		pidns_pid2str(PT_TGID), sprintrc(rc));
 
-	puts("+++ exited with 0 +++");
+	rc = syscall(__NR_getpriority, PRIO_PGRP,
+			  F8ILL_KULONG_MASK | pgid);
+	pidns_printf("getpriority(PRIO_PGRP, %s) = %ld\n",
+		pidns_pid2str(PT_PGID), rc);
+
+	pidns_printf("+++ exited with 0 +++\n");
 	return 0;
 }
 
