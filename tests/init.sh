5589370658af84355ce6e965ea8ef218a4a78b1f
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-06-26 20:13:28 +0200

    Implement testing framework for pidns
    * tests/pidns.c: New file.
    * tests/pidns.h: New file.
    * tests/Makefile.am (libtests_a_SOURCES): Add pidns.c.
    * tests/init.sh (test_pidns, test_pidns_run_strace): New functions.

--- tests/init.sh
+++ tests/init.sh
@@ -387,6 +387,36 @@ test_prog_set()
 	test_pure_prog_set "$@" < "$srcdir/$NAME.in"
 }
 
+test_pidns_run_strace()
+{
+	local parent_pid
+
+	check_prog tail
+	check_prog cut
+	check_prog grep
+
+	run_prog > /dev/null
+	run_strace --pidns-translation -f -e signal=!SIGKILL $@ $args > "$EXP"
+
+	#filter out logs made by the parent process of the pidns test
+	parent_pid="$(tail -n 1 $LOG | cut -d' ' -f1)"
+	grep -E -v "^$parent_pid " "$LOG" > "$OUT"
+	match_diff "$OUT" "$EXP"
+}
+
+test_pidns()
+{
+	#ioctl(NS_GET_PARENT) is added in Linux 4.9
+	require_min_kernel_version_or_skip 4.9
+	check_prog unshare
+
+	test_pidns_run_strace "$@"
+
+	#test PID translation when /proc is mounted from an other namespace
+	STRACE="unshare -Urpf $STRACE"
+	test_pidns_run_strace "$@"
+}
+
 check_prog cat
 check_prog rm
 
