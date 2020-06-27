8bb039fc7a5fe9358ff6f26e43bce80bb0f94b02
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-06-27 13:06:29 +0200

    add comments to tests/pidns.h

--- tests/pidns.h
+++ tests/pidns.h
@@ -21,11 +21,21 @@ enum pid_type {
 	PT_NONE = -1
 };
 
+/* Our PIDs in our namespace */
 extern pid_t pidns_ids[PT_COUNT];
+/* Our PIDs in strace's namespace */
 extern pid_t pidns_strace_ids[PT_COUNT];
 
+/* Prints leader (process tid) before printf */
 void pidns_printf(const char *format, ...);
+
+/*
+ * Returns a static buffer containing the string that is expected to be printed
+ * by strace when printing our PID
+ */
 const char *pidns_pid2str(enum pid_type type);
+
+/* Should be called at the beginning of the test's main function */
 void pidns_test_init(void);
 
 #endif
